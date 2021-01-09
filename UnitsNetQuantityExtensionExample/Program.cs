39416ae3d6d84b07ff9e4c605b1ef3d6e4e9d741
Ruud Poutsma <Ruud.Poutsma@priva.com>
2021-01-09 16:00:05 +0100

    add quantities and geenrate

--- UnitsNetQuantityExtensionExample/Program.cs
+++ UnitsNetQuantityExtensionExample/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using UnitsNet.CodeGen;
 
 namespace UnitsNetQuantityExtensionExample
 {
@@ -10,6 +7,42 @@ namespace UnitsNetQuantityExtensionExample
     {
         static void Main(string[] args)
         {
+            Generate();
+        }
+
+        private static void Generate()
+        {
+            var basePath = Directory.GetCurrentDirectory();
+            var baseProjectpath = Path.Combine(basePath, "../../../");
+            var customPath = Path.Combine(baseProjectpath, "Custom");
+            var sourceFolderPath = Path.Combine(customPath, "UnitDefinitions");
+            var quantitiesTargetFolderPath = Path.Combine(customPath, "Quantities");
+            var unitsTargetFolderPath = Path.Combine(customPath, "Units");
+            var quantityfactoryFolderPath = customPath;
+            var options = new Options
+            {
+                LogVerbose = true,
+                SourceFolderPath = sourceFolderPath,
+                UseNullableReferenceTypes = false,
+                Quantities = new GenerateOptions
+                {
+                    NamespaceName = "UnitsNetQuantityExtensionExample.Custom.Quantities",
+                    TargetFolderPath = quantitiesTargetFolderPath
+                },
+                Units = new GenerateOptions
+                {
+                    NamespaceName = "UnitsNetQuantityExtensionExample.Custom.Units",
+                    TargetFolderPath = unitsTargetFolderPath
+                },
+                StaticQuantityFactory = new GenerateOptions
+                {
+                    NamespaceName = "UnitsNetQuantityExtensionExample.Custom",
+                    TargetFilename = "PetroleumEngineeringQuantities",
+                    TargetFolderPath = quantityfactoryFolderPath
+                },
+            };
+            var generator = new ExtensionsCodeGen(options);
+            generator.Generate();
         }
     }
 }
